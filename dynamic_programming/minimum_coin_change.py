fbfffbdcaa55d80754200b44f6ee5a56422026ca
sun-hainan <55427017+sun-hainan@users.noreply.github.com>
2026-04-18 13:27:32 +0800

    Add Chinese comments to minimum_coin_change.py

--- dynamic_programming/minimum_coin_change.py
+++ dynamic_programming/minimum_coin_change.py
@@ -1,39 +1,67 @@
 """
-You have m types of coins available in infinite quantities
-where the value of each coins is given in the array S=[S0,... Sm-1]
-Can you determine number of ways of making change for n units using
-the given types of coins?
-https://www.hackerrank.com/challenges/coin-change/problem
+硬币找零问题 (Coin Change Problem) - 中文注释版
+==========================================
+
+问题定义：
+    给定 m 种面值的硬币（每种无限枚），和一个金额 n，
+    计算凑出金额 n 的不同方法数。
+
+LeetCode/HackerRank 经典题目。
+
+动态规划思路：
+    定义 dp[i] = 凑出金额 i 的方法数
+
+    状态转移：
+        对于每种硬币 coin_val，遍历所有可能的金额 j
+        dp[j] += dp[j - coin_val]
+
+    解释：当我们决定用面值为 coin_val 的硬币时，
+    剩余金额 j - coin_val 的方法数就是 dp[j - coin_val]
+
+    base case：dp[0] = 1（凑出金额 0 只有一种方法：什么都不选）
+
+时间复杂度：O(m * n)，m 为硬币种类数，n 为金额
+空间复杂度：O(n)
 """
 
 
 def dp_count(s, n):
     """
-    >>> dp_count([1, 2, 3], 4)
-    4
-    >>> dp_count([1, 2, 3], 7)
-    8
-    >>> dp_count([2, 5, 3, 6], 10)
-    5
-    >>> dp_count([10], 99)
-    0
-    >>> dp_count([4, 5, 6], 0)
-    1
-    >>> dp_count([1, 2, 3], -5)
-    0
+    硬币找零 - DP 解法
+
+    参数:
+        s: 硬币面值列表，如 [1, 2, 3]
+        n: 目标金额
+
+    返回:
+        凑出金额 n 的方法数
+
+    示例:
+        >>> dp_count([1, 2, 3], 4)
+        4
+        # 4 = {1,1,1,1}, {1,1,2}, {1,3}, {2,2}
+
+        >>> dp_count([1, 2, 3], 7)
+        8
+        >>> dp_count([2, 5, 3, 6], 10)
+        5
+        >>> dp_count([10], 99)
+        0
+        >>> dp_count([4, 5, 6], 0)
+        1
     """
     if n < 0:
         return 0
-    # table[i] represents the number of ways to get to amount i
+
+    # dp[i] = 凑出金额 i 的方法数
     table = [0] * (n + 1)
 
-    # There is exactly 1 way to get to zero(You pick no coins).
+    # base case：凑出金额 0 只有一种方法（什么都不选）
     table[0] = 1
 
-    # Pick all coins one by one and update table[] values
-    # after the index greater than or equal to the value of the
-    # picked coin
+    # 遍历每种硬币
     for coin_val in s:
+        # 用当前硬币能凑出的金额
         for j in range(coin_val, n + 1):
             table[j] += table[j - coin_val]
 
@@ -42,5 +70,4 @@ def dp_count(s, n):
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
