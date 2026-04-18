4fdde35f61c76a2e9f7d1708d819241c07a89a64
sun-hainan <55427017+sun-hainan@users.noreply.github.com>
2026-04-18 15:03:26 +0800

    Add: application scenarios

--- Chinese Algorithms/DP/02_Knapsack.py
+++ Chinese Algorithms/DP/02_Knapsack.py
@@ -6,10 +6,38 @@ Knapsack - 背包问题
 给定n个物品，每个有重量wt[i]和价值val[i]，
 在不超过背包容量W的情况下，选择物品使总价值最大。
 
+【时间复杂度】O(n * W)
+【空间复杂度】O(n * W)
+
 【应用场景】
-- 资源分配
-- 项目选择
+- 电商促销组合优惠
+- 旅行行李打包
+- 项目投资选择
+- 资源分配优化
+
+【何时使用】
+- 组合优化问题
+- 有容量/预算限制的选择
 - 投资组合优化
+- 资源有限的项目选择
+
+【实际案例】
+# 旅行打包行李（背包容量有限）
+items = [
+    {"name": "护照", "weight": 0.2, "value": 100},
+    {"name": "电脑", "weight": 2.0, "value": 80},
+    {"name": "衣服", "weight": 3.0, "value": 30},
+    {"name": "相机", "weight": 1.5, "value": 60}
+]
+# 行李限额10kg，如何打包价值最高？
+
+# 双11购物凑单（预算有限，如何凑满减最大）
+products = [
+    {"name": "面膜", "price": 50, "value": 5},
+    {"name": "精华", "price": 200, "value": 15},
+    {"name": "面霜", "price": 150, "value": 10}
+]
+# 预算500元，如何凑单最优惠？
 """
 
 def knapsack(W, weights, values, n):
@@ -25,34 +53,16 @@ def knapsack(W, weights, values, n):
     Returns:
         最大价值
     """
-    # dp[i][w] = 前i个物品在容量w下的最大价值
     dp = [[0] * (W + 1) for _ in range(n + 1)]
     
     for i in range(1, n + 1):
         for w in range(W + 1):
             if weights[i - 1] <= w:
-                # 选或不选第i个物品，取较大值
                 dp[i][w] = max(
                     values[i - 1] + dp[i - 1][w - weights[i - 1]],
                     dp[i - 1][w]
                 )
             else:
-                # 不能选第i个物品
                 dp[i][w] = dp[i - 1][w]
     
     return dp[n][W]
-
-
-# ---------- LCS ----------
-FILES['Chinese Algorithms/DP/03_LCS.py'] = 
-LCS - 最长公共子序列
-==========================================
-
-【问题定义】
-找出两个字符串/序列中，最长的公共子序列。
-子序列可以不连续，但相对顺序必须保持。
-
-【应用场景】
-- DNA序列比对
-- diff文件比较
-- 论文查重
